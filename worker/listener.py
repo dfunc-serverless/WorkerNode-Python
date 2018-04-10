97998f028ea3700a12ce00232be6d5fa8ed1ca00
bowenislandsong <bowenislandsong@gmail.com>
2018-04-09 21:47:09 -0400

    listener pre done

--- worker/listener.py
+++ worker/listener.py
@@ -1,28 +1,70 @@
 """
 TODO:
-Listening to Server call
-Get Job ID
-Get Triger 
+1. Register at http://www.defunc.tech
+request
+Http GET dfunc.tech/worker/(apikey: from worker user input)
+save to relative local place
 
+2. listern
+Listening to Server call
+
+Worker ID : subscription_name
+msg:  Job ID
+
+3. Run Docker
 Get Image name
-Get Triger
+
+4. Return result
+Send msg
+5. Get paid (optional)
 dependencies: Docker installed, Python 2.7 or 3.
 """
 
 
 import argparse
 import time
-
+import json
+import os.path
 from google.cloud import pubsub_v1
 
-def receive_messages(project, subscription_name):
+# Global Variables
+GoogleAPISecretPath = './GoogleAPISecret.json'
+workerAPIkeyPath = './workerAPIkey.json'
+
+def setUserkey():
+    workerAPIkey = raw_input('Input APIkey (Aquire your APIkey at "http://www.defunc.tech"):')
+    with open(workerAPIkeyPath, 'w') as outfile:
+        json.dump(workerAPIkey, outfile)
+    return workerAPIkey
+
+def setGoogleAPI():
+    googleAPIkey = raw_input('Input absolute path (including filename) of Google JSON secret downloaded from "http://www.defunc.tech" on register):')
+    os.rename(googleAPIkey, GoogleAPISecretPath)
+    if (keyexist(GoogleAPISecretPath)):
+        getGoogleAPI()
+
+def getGoogleAPI():
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.abspath(GoogleAPISecretPath)
+
+def keyexist(key):
+    return os.path.exists(key) 
+
+def getUserkey():
+    return json.load(open(workerAPIkeyPath))
+
+def receive_messages_with_custom_attributes(project, subscription_name):
     """Receives messages from a pull subscription."""
     subscriber = pubsub_v1.SubscriberClient()
     subscription_path = subscriber.subscription_path(
         project, subscription_name)
 
     def callback(message):
-        print('Received message: {}'.format(message))
+        print('Received message: {}'.format(message.data)) # comment out later
+        if message.attributes:
+            print('Attributes:') # comment out later
+            for key in message.attributes:
+                value = message.attributes.get(key)
+                print('{}: {}'.format(key, value)) # comment out later
         message.ack()
 
     subscriber.subscribe(subscription_path, callback=callback)
@@ -33,5 +75,26 @@ def receive_messages(project, subscription_name):
     while True:
         time.sleep(60)
 
+def workerMain():
+    # worker ID 
+    if (keyexist(workerAPIkeyPath)):
+        workerAPIkey = getUserkey()
+    else:
+        workerAPIkey = setUserkey()
+        pass
+    print "Your Worker ID: " + workerAPIkey
+    # google API secret
+    if (keyexist(GoogleAPISecretPath)):
+        getGoogleAPI()
+    else:
+        setGoogleAPI()
+    # Start listening
+    receive_messages_with_custom_attributes('dfunc',workerAPIkey)
+
+
+
+
+
 if __name__ == "__main__":
-    receive_messages("place holder","what")
+    workerMain()
+    #receive_messages("dfunc-bu","what")
